5b20ecc4cece4930910ad9cd4970cf636df928e5
Uzum Stanley <70582317+uzumstanley@users.noreply.github.com>
2025-02-21 14:07:40 +0000

    Add files via upload

---
+++ eda_tool/outlier_detection.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+def detect_outliers(df, col):
+    Q1 = df[col].quantile(0.25)
+    Q3 = df[col].quantile(0.75)
+    IQR = Q3 - Q1
+    lower_bound = Q1 - 1.5 * IQR
+    upper_bound = Q3 + 1.5 * IQR
+    return df[(df[col] < lower_bound) | (df[col] > upper_bound)]
